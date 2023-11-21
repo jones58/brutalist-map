01a82f057589c9542b5b4e692da4c6ef54e8013a
jones58 <108466184+jones58@users.noreply.github.com>
2023-11-21 16:44:19 +0000

    edit color scheme of map with light/dartk mode btn

--- scripts.js
+++ scripts.js
@@ -2,11 +2,24 @@ if (!mapboxgl.accessToken) {
   mapboxgl.accessToken = mapboxToken;
 }
 
-var map = new mapboxgl.Map({
+let map = new mapboxgl.Map({
   container: "map",
   center: [-0.1278, 51.5074],
   zoom: 10,
-  style: "mapbox://styles/jones581/clp708iyk00o801qu8118305m",
+  style: "mapbox://styles/jones581/clp8fym2g01u901qmbpzq0dde",
 });
 
-lightMode;
+const lightModeToggle = document.getElementById("light-mode");
+const darkModeToggle = document.getElementById("dark-mode");
+
+lightModeToggle.addEventListener("click", () => {
+  map.setStyle("mapbox://styles/mapbox/light-v10");
+  lightModeToggle.classList.add("hidden");
+  darkModeToggle.classList.remove("hidden");
+});
+
+darkModeToggle.addEventListener("click", () => {
+  map.setStyle("mapbox://styles/jones581/clp8fym2g01u901qmbpzq0dde");
+  lightModeToggle.classList.remove("hidden");
+  darkModeToggle.classList.add("hidden");
+});
